128e0d1b0fb87c67403d144b2c65282795028dd0
Francesco Romeo <romeo.francesco.2002@gmail.com>
2023-10-27 16:12:54 +0200

    Delete [01]LT_21-22 directory

--- [01]LT_21-22/SEM2/ASD/Exercises/Recursives/pow.py
+++
@@ -1,7 +0,0 @@
-def pow(a,b):
-    if b == 0:
-        return 1
-    else:
-        return a * pow(a,b-1)
-
-print(pow(5, 2));
--- [01]LT_21-22/SEM2/ASD/Exercises/Recursives/print_decimal_one_digit.py
+++
@@ -1,8 +0,0 @@
-def print_decimal_one_digit(n):
-    if n == 0:
-        pass
-    else:
-        print_decimal_one_digit(n // 10)
-        print(n % 10, end="")
-
-print_decimal_one_digit(293)
